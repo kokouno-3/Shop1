25d75dd7d447d663787330eb4c65d83c3e99438e
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-11-01 09:24:03 +0000

    [Add]ヘッダーの追加、マイページの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,28 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller? #(customer側の新規登録情報を保存)
+
 
   def after_sign_out_path_for(resource)
-    new_admin_session_path # ログアウト後に遷移するpathを設定(admin側)
+    # case resource
+      # when Admin
+         new_admin_session_path # ログアウト後に遷移するpathを設定(admin側)
+    #   when Customer
+    #     about_path  # ログアウト後に遷移するpathを設定(customer側)
+    # end
   end
 
    def after_sign_in_path_for(resource)
-    admins_path # ログアウト後に遷移するpathを設定(admin側)
+     case resource
+       when Admin
+         admins_path # ログイン後に遷移するpathを設定(admin側)
+       when Customer
+         about_path  # ログイン後に遷移するpathを設定(customer側)
+     end
    end
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :postcode, :address, :phonenumer])
+  end
 end
--- app/controllers/publics/customers_controller.rb
+++ app/controllers/publics/customers_controller.rb
@@ -1,12 +1,32 @@
 class Publics::CustomersController < ApplicationController
+
   def show
+    @customer = Customer.find(params[:id])
   end
+
   def edit
+    @customer = Customer.find(params[:id])
   end
+
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to publics_customer_path(current_customer.id)
   end
+
   def unsubscribe
   end
+
   def withdraw
+    @customer = Customer.find(params[:id])
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
+
+private
+  def customer_params
+    params.require(:customer).permit([:first_name, :last_name, :first_name_kana, :last_name_kana, :postcode, :address, ])
   end
 end
