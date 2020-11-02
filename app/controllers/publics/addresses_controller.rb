6a82c436330463a899335362dcb480fb50b3b706
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-11-02 07:43:14 +0000

    [Add]配送先作成、マイページ編集の追加

--- app/controllers/publics/addresses_controller.rb
+++ app/controllers/publics/addresses_controller.rb
@@ -1,16 +1,31 @@
 class Publics::AddressesController < ApplicationController
   def index
+    @addresses = Address.all
+    @address = Address.new
+    @customer = Customer.find(params[:customer_id])
   end
 
   def create
+    @customer = Customer.find(params[:customer_id])
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @address.save
+    redirect_to publics_customer_addresses_path
   end
 
   def edit
+    @address = Address.find(params[:id])
+    @customer = Customer.find(params[:customer_id])
   end
 
   def destroy
   end
 
   def update
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:postcode, :address, :name, :customer_id)
   end
 end
