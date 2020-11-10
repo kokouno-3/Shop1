447b2819c8380f9bcef8c474895069cbe35439f2
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-11-10 08:06:55 +0000

    [Fix]注文の際に、新しい配送先を入力すると、配送先一覧に反映されるよう修正

--- app/controllers/publics/orders_controller.rb
+++ app/controllers/publics/orders_controller.rb
@@ -45,6 +45,8 @@ class Publics::OrdersController < ApplicationController
       @order_detail.save
     end
     current_customer.carts.destroy_all
+
+    Address.create!(customer_id: @customer.id, postcode: @order.postcode, address: @order.address, name: @order.name)
   end
 
   def index
